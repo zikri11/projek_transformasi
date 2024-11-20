c14f40cc37d986663db2f24f1273b6216c86db58
Zikri11 <Naril4860@gmail.com>
2024-11-20 11:11:39 +0700

    Update app.py to set host and port

--- app.py
+++ app.py
@@ -61,4 +61,5 @@ def process_image():
                            transformed_image=url_for('static', filename='uploads/transformed.png'))
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
+
